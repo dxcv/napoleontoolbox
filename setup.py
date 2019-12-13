8cec63318c0f5506f96510701120557bb2ce0563
stef564 <stefan@napoleonx.ai>
2019-12-13 10:37:35 +0100

    widget module

--- setup.py
+++ setup.py
@@ -54,9 +54,9 @@ build_requires = [
 
 setup(
     name='napoleontoolbox',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
-    download_url='https://github.com/stef564/napoleontoolbox/archive/0.2.1.tar.gz',
+    download_url='https://github.com/stef564/napoleontoolbox/archive/0.2.2.tar.gz',
     author='Napoleon Group',
     author_email='dsi@napoleonx.ai',
     description='Dashboard for financial market data',
